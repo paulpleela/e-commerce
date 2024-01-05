c77033075e9128e9dbf996a527a4012aca364172
unknown <paulpleela@gmail.com>
2024-01-05 23:15:44 +0700

    Added Sidebars

---
+++ src/components/navbar/sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import styles from "./sidebar.module.scss";
+import Link from "next/link";
+
+const Sidebar = ({toggleSidebar}) => {
+  return (
+    <aside className={styles.sidebar} id="sidebar">
+      <button onClick={toggleSidebar}>
+        <svg
+          className={styles.close}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M4.29289 8.29289C4.68342 7.90237 5.31658 7.90237 5.70711 8.29289L12 14.5858L18.2929 8.29289C18.6834 7.90237 19.3166 7.90237 19.7071 8.29289C20.0976 8.68342 20.0976 9.31658 19.7071 9.70711L12.7071 16.7071C12.3166 17.0976 11.6834 17.0976 11.2929 16.7071L4.29289 9.70711C3.90237 9.31658 3.90237 8.68342 4.29289 8.29289Z"
+            ></path>{" "}
+          </g>
+        </svg>
+      </button>
+      <Link href="/">Home</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/order">Your Order</Link>
+      <Link href="/contact">Contact Us</Link>
+    </aside>
+  );
+};
+
+export default Sidebar;
